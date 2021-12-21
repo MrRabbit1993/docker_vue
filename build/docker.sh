bcb8d0c0170acb3a82359bd6246400cca49b76b6
MrRabbit <1993iweb@gmail.com>
2021-12-21 12:35:53 +0800

    feat: npm script部署docker

---
+++ build/docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dockerImages="vuets"
+version="0.1"
+prot="8087"
+nginxProt="8081"
+containerName="textcontainer"
+
+echo "===准备创建docker镜像
+=====>"
+
+docker build -t $dockerImages:$version -f $1 .
+
+echo "===创建$dockerImages:$version镜像成功
+=====>"
+
+docker image  ls | grep $dockerImages
+
+echo "===准备创建运行容器
+====>"
+
+docker run -d -p $prot:$nginxProt --name $containerName $dockerImages:$version
