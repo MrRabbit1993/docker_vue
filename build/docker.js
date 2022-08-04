57d6797be225604376aeac41e610951ecaa675d5
MrRabbit <oujian@citycloud.com.cn>
2022-08-04 18:07:46 +0800

    feat: 格式

--- build/docker.js
+++ build/docker.js
@@ -1,11 +1,12 @@
 const execa = require('execa')
-const { name, version } = require('../package.json')
+const { name,version } = require('../package.json')
 const port = 8087
 const nginxPort = 8081
+
 function runImage() {
   console.log('构建成功')
   // docker run -d -p $prot:$nginxProt --name $containerName $dockerImages:$version
-  return execa('docker',[ 'run','-d','-p',`${port}:${nginxPort}`,'--name','docker_demo',name])
+  return execa('docker', ['run', '-d', '-p', `${port}:${nginxPort}`, '--name', 'docker_demo', name])
 }
 execa('docker', ['build', `.`, `-t`, `${name}`], { stdio: 'inherit' })
   .then(runImage)
